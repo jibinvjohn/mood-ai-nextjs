7373e77bb59c3ee5474b11d0838e7816fccb3f14
Jibin <jibin_1@hotmail.com>
2024-08-05 23:51:17 +0530

    auth sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from '@clerk/nextjs/dist/types/components.server'
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
